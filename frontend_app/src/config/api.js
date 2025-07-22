6c83ad5a432865702a9da3d4b0beeeb37cbd4ed3
isha152002 <ishajain152002@gmail.com>
2025-07-22 22:25:40 +0530

    Initial commit: added project

---
+++ frontend_app/src/config/api.js
@@ -0,0 +1,14 @@
+const BACKEND_URL = 'https://face-auth-backend.onrender.com'
+
+// BASE AUTH
+const AUTH_URL = '/api/auth'
+// BASE FACENET
+const IMAGE_URL = '/api/image'
+
+export const REGISTER_URL =         BACKEND_URL + AUTH_URL + '/register'
+export const LOGIN_URL =            BACKEND_URL + AUTH_URL + '/login'
+export const LOGOUT_URL =           BACKEND_URL + AUTH_URL + '/logout'
+
+export const GET_IMAGE_URL =        BACKEND_URL + IMAGE_URL + '/get/from/url'
+export const GET_PROFILE_PIC =      BACKEND_URL + IMAGE_URL + '/get/profile/pic'
+export const GET_SIMILARITY_URL =   BACKEND_URL + IMAGE_URL + '/get/matches'
